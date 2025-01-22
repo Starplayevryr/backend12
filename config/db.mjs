55535f345dcf93556c8e51c3a4dd388559d08074
Keerthana R <148705349+Starplayevryr@users.noreply.github.com>
2025-01-22 16:06:36 +0530

    removes .connect

--- config/db.mjs
+++ config/db.mjs
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDb = async()=>{
     try{
-        await mongoose.connect('mongodb+srv://kirthiyamini826:London123@cluster0.ezs0z.mongodb.net/TestDB')
+        await mongoose.connect('')
         console.log("Connected to database");
     }
 
